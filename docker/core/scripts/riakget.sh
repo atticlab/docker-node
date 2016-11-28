9e777823cceaf13a8c4ba47d2c558308912bad7d
eugene <we.danced.a.lot@gmail.com>
2016-11-28 16:44:35 +0200

    scripts merge

--- docker/core/scripts/riakget.sh
+++ docker/core/scripts/riakget.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-BUCKET_NAME="history"
-LOGS_DIR="logs"
-LOG_FILENAME="get.log"
-
 if [ -z "$1" ]
   then
     echo "Usage get.sh PATH FILENAME"
@@ -18,12 +14,9 @@ fi
 
 FILE_TO_GET=$2
 DEST_PATH=$(echo $1 | sed 's/\//\%2f/g')
-RIAK_COMMAND="buckets/${BUCKET_NAME}/keys/${DEST_PATH}"
+RIAK_COMMAND="buckets/${RIAK_BUCKET}/keys/${DEST_PATH}"
 
 FULL_CMD="curl -sf ${RIAK_HOST}${RIAK_COMMAND} -o $2"
 
-#create logs dir
-mkdir -p ${LOGS_DIR}
-
 #run the command
-(eval "${FULL_CMD}") >> ./${LOGS_DIR}/${LOG_FILENAME} 2>&1
+(eval "${FULL_CMD}") > /dev/null 2>&1
--- docker/core/scripts/riakput.sh
+++ docker/core/scripts/riakput.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-BUCKET_NAME="history"
-LOGS_DIR="logs"
-LOG_FILENAME="put.log"
-
 if [ -z "$1" ]
   then
     echo "Usage put.sh FILENAME PATH"
@@ -18,16 +14,13 @@ fi
 
 FILE_TO_SEND=$1
 DEST_PATH=$(echo $2 | sed 's/\//\%2f/g')
-RIAK_COMMAND="buckets/${BUCKET_NAME}/keys/${DEST_PATH}?returnbody=false"
+RIAK_COMMAND="buckets/${RIAK_BUCKET}/keys/${DEST_PATH}?returnbody=false"
 HEADER="Content-Type: multipart/mixed"
 
 FULL_CMD="curl -v -XPUT  --data-binary @${FILE_TO_SEND} -H \"${HEADER}\" ${RIAK_HOST}${RIAK_COMMAND}"
 
-#create logs dir
-mkdir -p ${LOGS_DIR}
-
 #set bucket props
-(curl -v -XPUT ${RIAK_HOST}buckets/${BUCKET_NAME}/props -H "Content-Type: application/json" -d '{"props":{"allow_mult":'false'}}') >> ./${LOGS_DIR}/${LOG_FILENAME} 2>&1
+(curl -v -XPUT ${RIAK_HOST}buckets/${RIAK_BUCKET}/props -H "Content-Type: application/json" -d '{"props":{"allow_mult":'false'}}') > /dev/null 2>&1
 
 #run the command
-(eval "${FULL_CMD}") >> ./${LOGS_DIR}/${LOG_FILENAME} 2>&1
+(eval "${FULL_CMD}") > /dev/null 2>&1
